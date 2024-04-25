57b72657f1a05f0066edb5bb3f638c9a441f1514
ilya k <ilya.k423@gmail.com>
2024-04-25 03:50:22 +0300

    chore: learn typeclasses

--- app/Main.hs
+++ app/Main.hs
@@ -4,3 +4,37 @@ import Lib
 
 main :: IO ()
 main = someFunc
+
+data RhType = Pos | Neg
+
+data ABOType = O | A | B | AB
+
+data BloodType = BloodType ABOType RhType
+
+type FullName = (String, String)
+
+data Patient = Patient
+  { fstName :: String,
+    sndName :: String,
+    age :: Int,
+    bloodType :: BloodType,
+    fullName :: FullName,
+    diagnosis :: String
+  }
+
+newPatient :: String -> String -> Int -> BloodType -> String -> Patient
+newPatient fstName sndName age bloodType diagnosis =
+  Patient
+    { fstName = fstName,
+      sndName = sndName,
+      age = age,
+      bloodType = bloodType,
+      diagnosis = diagnosis,
+      fullName = (fstName, sndName)
+    }
+
+p1 :: Patient
+p1 = newPatient "John" "Doe" 30 (BloodType AB Pos) "gastritis"
+
+class Analysis a where
+  getBloodType :: Patient -> BloodType
